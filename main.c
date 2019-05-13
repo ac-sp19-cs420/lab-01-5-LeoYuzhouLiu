945157dde18032510c62191673ddf3dfea6fc443
LeoYuzhouLiu <47706180+LeoYuzhouLiu@users.noreply.github.com>
2019-05-12 21:27:30 -0500

    lab 1.5 demo

--- main.c
+++ main.c
@@ -1,123 +1,183 @@
 #include <stdio.h>
 #include <stdlib.h>
+//#include <iostream>
 
 /*
-This class builds a doubly-linked-list and a circular buffer.
-I'm still unfamilier with parameters and classes in c, 
-so errors are reported for some of my parameters and items, 
-and I don't know how to solve them. 
+This class builds a linked list, and it builds a stack and a queue.
 
 @author Leo Yuzhou Liu
 */
+//using namespace std;
 
-/*
-Double Linked-list
-*/
-typedef struct DLL1 {
+struct Node {
 	int data;
 	int key;
-	DLL1* next;
-	DLL1* last;
-} DLL1;
+	struct Node* next;
+};
 
-typedef struct DLL2 {
-	DLL1* head;
-} DLL2;
-
-typedef struct CirBuf {
-	DLL1* head;
-	DLL1* tail;
-	int size;
-	int num;
-} CirBuf;
-
-// Here are a few simple methods for me to operate later.
-int head(CirBuf* cb) {
-	if (cb->head != NULL) {
-		return cb->head->data;
-	}
-	return -1;
-}
-
-int tail(CirBuf* cb) {
-	if (cb->tail != NULL) {
-		return cb->tail->data;
-	}
-	return -1;
-}
-
-int pop(CirBuf* cb) {
-	if (cb->tail == NULL) {
-		return -1;
-	}
-	DLL1* nodePtr = cb->tail;
-	cb->tail = cb->tail->last;
-	nodePtr->last = NULL;
-	nodePtr->next = NULL;
-	int val = nodePtr->data;
-	free(nodePtr);
-	return val;
-}
-
-DLL2 insert(DLL2 d, int key, int data) {
-	DLL1* newNode = (DLL1*)malloc(sizeof(DLL1));
-	newNode->data = data;
-	newNode->last = NULL;
-	newNode->next = NULL;
-	if (d->head == NULL) {
-		d->head = newNode;
-	}
-	else {
-		d->head->last = newNode;
-		newNode->next = d->head;
-		d->head = newNode;
-	}
-	return d;
-}
+typedef struct Head {
+	struct Node* head;
+}Head;
 
 /*
-insert_dll() adds a new empty node gives it values.
+The insert() determines if the node conflicts with any existing nodes first.
+If not, it grabs the 'next' of previous node and hold it in to irself,
+then connects it's 'next' to the next node.
 */
-int insert_dll(Head head, int data, int key) {
-	struct DLL1* nodePtr = head.head;
-	struct DLL1* last = nodePtr;
+int insert(Head head, int data, int key) {
+	struct Node* nodePtr = head.head;
+	struct Node* prev = nodePtr;
 
 	while (nodePtr->next != NULL) {
 		if (nodePtr->key == key) {
 			return 0;
 		}
 		if (key < nodePtr->key) {
-			// sorting
-			nodePtr->next = last->next;
-			last->next = nodePtr;
+			nodePtr->next = prev->next;
+			prev->next = nodePtr;
 			nodePtr->data = data;
 			nodePtr->key = key;
 		}
-		last = nodePtr;
+		prev = nodePtr;
+		nodePtr = nodePtr->next;
+	}
+
+}
+
+/*
+The remove() locates the node then does the opposite thing of inset().
+*/
+int remove(Head head, int data, int key) {
+	struct Node* nodePtr = head.head;
+	struct Node* prev = nodePtr;
+
+	while (nodePtr->next != NULL) {
+		if (nodePtr->key == key) {
+			prev->next = nodePtr->next;
+			return 1;
+		}
+
+		prev = nodePtr;
+		nodePtr = nodePtr->next;
+	}
+	return 0;
+}
+
+void print_list(Head head) {
+	struct Node* nodePtr = head.head;
+	while (nodePtr != NULL) {
+		// cout << nodePtr->data << "\t";
+		printf("%d\t", nodePtr->data);
 		nodePtr = nodePtr->next;
 	}
 }
 
-// I don't know if that's how to return -1.
-int is_in_list_dll(Head head, int key) {
-	struct DLL1* nodePtr = head.head;
-	if (nodePtr == NULL) {
+/*
+create_array() gives the linked-list properties of an array.*/
+int* create_array(Head head) {
+	struct Node* nodePtr = head.head;
+	int size = 0;
+	while (nodePtr != NULL) {
+		size++;
+		nodePtr = nodePtr->next;
+	}
+	// int* result = new int[size];
+	int* result = (int*)malloc(sizeof(int) * size);
+	nodePtr = head.head;
+	int pos = 0;
+	while (nodePtr != NULL) {
+		result[pos] = nodePtr->data;
+		pos++;
+		nodePtr = nodePtr->next;
+	}
+	return result;
+}
+
+/*
+push_stack() creates a temporary head which contains desired data,
+and then turn the original head into a normal node,
+then connects the new head(temp) with it.
+*/
+void push_stack(Head head, int val) {
+	struct Node* nodePtr = head.head;
+	struct Node* temp = head.head;
+	temp->data = val;
+	temp->next = nodePtr;
+	head.head = temp;
+}
+
+/*
+Redefine the head node.
+*/
+void pop_stack(Head head) {
+	struct Node* nodePtr = head.head;
+	head.head = head.head->next;
+}
+
+/*
+is_empty() loops the stack and returns 0 once it detects any non-null value.
+It can be used by both the stack and the queue.
+*/
+int is_empty(Head head) {
+	struct Node* nodePtr = head.head;
+	if (nodePtr->data != NULL) {
 		return 0;
 	}
+	return 1;
+}
+
+/*
+This does the same thing as push_stack()
+*/
+void push_queue(Head head, int val) {
+	struct Node* nodePtr = head.head;
+	struct Node* temp = head.head;
+	temp->data = val;
+	temp->next = nodePtr;
+	head.head = temp;
+}
+
+/*
+pop_queue() locates the last three node of the queue
+and make the last two node points to NULL.
+*/
+void pop_queue(Head head) {
+	struct Node* nodePtr = head.head;
+	while (nodePtr->next->next == NULL) {
+		nodePtr->next = NULL;
+		nodePtr = nodePtr->next;
+	}
+}
+
+/*
+Loops the list and detect the target node.
+*/
+int is_in_list(Head head, int key) {
+	struct Node* nodePtr = head.head;
 	while (nodePtr != NULL) {
 		if (nodePtr->key == key) {
 			return 1;
 		}
 		nodePtr = nodePtr->next;
 	}
-	
-	return -1;
-
+	return 0;
 }
 
-// find_element_dll() uses a while loop to find elements.
-int find_element_dll(Head head, int key) {
-	struct DLL1* nodePtr = head.head;
+/*
+last() locates the last node and returns its data.
+*/
+int last(Head head) {
+	struct Node* nodePtr = head.head;
+	while (nodePtr->next == NULL) {
+		return nodePtr->data;
+		nodePtr = nodePtr->next;
+	}
+}
+
+/*
+Locates the node and return its data.*/
+int find(Head head, int key) {
+	struct Node* nodePtr = head.head;
 	while (nodePtr != NULL) {
 		if (nodePtr->key == key) {
 			return nodePtr->data;
@@ -127,114 +187,14 @@ int find_element_dll(Head head, int key) {
 	return 0;
 }
 
-// opposite of insert_dll()
-int remove_dll(Head head, int key) {
-	struct DLL1* nodePtr = head.head;
-	struct DLL1* last = nodePtr;
-
-	while (nodePtr->next != NULL) {
-		if (nodePtr->key == key) {
-			last->next = nodePtr->next;
-			return 1;
-		}
-		last = nodePtr;
-		nodePtr = nodePtr->next;
-	}
-	return 0;
-}
-
-// print_dll uses a while loop to print all elements.
-void print_dll(Head head) {
-	struct DLL1* nodePtr = head.head;
-	while (nodePtr != NULL) {
-		printf("%d\n", nodePtr->data);
-		nodePtr = nodePtr->next;
-	}
-}
-
-/*
-create_array_dll() gives the linked-list properties of an array.
-*/
-int* create_array_dll(Head head) {
-	struct DLL1* nodePtr = head.head;
-	int size = 0;
-	while (nodePtr != NULL) {
-		size++;
-		nodePtr = nodePtr->next;
-	}
-	int* result = new int[size];
-	// VS reports error for "new".
-	nodePtr = head.head;
-	int i = 0;
-	while (nodePtr != NULL) {
-		result[i] = nodePtr->data;
-		i++;
-		nodePtr = nodePtr->next;
-	}
-	return result;
-}
-
-
-
-/*
-Circular Buffer
-*/
-
-typedef struct circular_list {
-	int size;
-	int num;                   // num is the number of occupied slots 
-	int data;
-	circular_list* head;
-	circular_list* tail;
-} circular_list;
-
-int create_cl(int size, Head head, Tail tail) {
-	struct circular_list nodePtr = head.head;
-	nodePtr->size = head->size;
-	nodePtr->num = num;
-	// parse in num
-	if (head->size != 1) {
-		return 1;
-	}
-	return 0;
-}
-
-// I'm not sure how to add a new element into a circular buffer.
-int push_cl(Head head) {
-	if (cl->num < cl->size) {
-		struct circular_list* nodePtr = head.head;
-		struct circular_list* newElement = nodePtr;
-		head->next = nodePtr->next;
-		nodePtr->next = head;
-		num++;
-		return 0;
-	}
-	return -1;
-}
-
-int pop_cl(circular_list* cl, Head head) {
-	if (cl->head != NULL) {
-		struct circular_list* nodePtr = head.head;
-		struct circular_list* newElement = nodePtr;
-		head->tail->next = nodePtr->next;
-		/*
-		I don't understand why to advance the tail in pop_cl()
-		so I'm not sure what to parse in.
-		*/
-		head->num -= 1;
-		return cl->tail->data;
-	}
-	return -1;
-}
-
-int head_cl(circular_list* cl) {
-	if (cl->head != NULL) {
-		return cl->tail->data;
-	}
-	return -1;
-}
-
-
 int main() {
+	//struct Node* head = new struct Node;
+	struct Node* head = (struct Node*)malloc(sizeof(struct Node));
+
+	head->data = 10;
+	head->next = (struct Node*)malloc(sizeof(struct Node));
+	head->next->next = 0;
+	struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+	newNode->data;
 	return 0;
 }
